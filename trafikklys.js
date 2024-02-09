f1c030c9e6e43cb97ca796add8a71c83cd9ac153
Marte <martelysnes@gmail.com>
2024-02-09 22:50:12 +0100

    la til fargestyring ved onclick

---
+++ trafikklys.js
@@ -0,0 +1,35 @@
+let redlight;
+let orangelight;
+let greenlight;
+
+function main() {
+    redlight = document.getElementById("redlight")
+    orangelight = document.getElementById("orangelight")
+    greenlight = document.getElementById("greenlight")
+}
+
+function skru_av_lys(lys) {
+    lys.classList.add("av")
+}
+
+function skru_på_lys(lys) {
+    lys.classList.remove("av")
+}
+
+function skru_på_kun_en(lys) {
+    if (lys == redlight) {
+        skru_på_lys(redlight)
+        skru_av_lys(orangelight)
+        skru_av_lys(greenlight)
+    }
+    else if (lys == orangelight) {
+        skru_av_lys(redlight)
+        skru_på_lys(orangelight)
+        skru_av_lys(greenlight)
+    }
+    else if (lys == greenlight) {
+        skru_av_lys(redlight)
+        skru_av_lys(orangelight)
+        skru_på_lys(greenlight)
+    }
+}
